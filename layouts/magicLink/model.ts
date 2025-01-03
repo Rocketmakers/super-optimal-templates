27bdab12e705f62704e65f89c82e2582d77dce42
Amélie <amelie@Amelies-MacBook-Pro.local>
2025-01-03 10:46:43 +0000

    Adds user name in magic link email template

--- layouts/magicLink/model.ts
+++ layouts/magicLink/model.ts
@@ -4,14 +4,17 @@
  * @examples require(".").sampleData
  */
 export interface IModel {
+  name: string
   returnUrl: string
 }
 
 export const sampleData: IModel[] = [
   {
+    name: "Rocketmakers",
     returnUrl: "https://www.rocketmakers.com"
   },
   {
+    name: "Rocketmakers",
     returnUrl: "https://www.rocketmakers.com"
   },
 ]
